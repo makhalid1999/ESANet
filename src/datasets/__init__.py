8775b02f6c77be8919c9fa9e5fb36d9790eb9695
Muhammad Abdullah Khalid <48885205+makhalid1999@users.noreply.github.com>
2024-12-22 23:41:56 +0500

    Update __init__.py

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -2,7 +2,4 @@
 """
 .. codeauthor:: Daniel Seichter <daniel.seichter@tu-ilmenau.de>
 """
-from .cityscapes.pytorch_dataset import Cityscapes
 from .nyuv2.pytorch_dataset import NYUv2
-from .sunrgbd.pytorch_dataset import SUNRGBD
-from .scenenetrgbd.pytorch_dataset import SceneNetRGBD
